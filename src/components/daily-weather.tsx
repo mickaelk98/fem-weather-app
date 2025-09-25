31e1ec63c88ab032121b156243253d25e3d2a7a3
Mickael Keita <keitamickael@gmail.com>
2025-09-25 14:02:27 +0200

    add saily weather display zone

--- src/components/daily-weather.tsx
+++ src/components/daily-weather.tsx
@@ -1,3 +1,17 @@
+import sunIcon from "../assets/images/icon-sunny.webp";
+
 export function DailyWeather() {
-  return <div>DailyWeather</div>;
+  return (
+    <div className="daily-weather rounded-[20px] px-6 py-10 flex flex-col md:flex-row justify-between">
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <p className="text-[28px]">Berlin, Germany</p>
+        <p className="text-[18px]">Tuesday, Aug 5, 2025</p>
+        <p></p>
+      </div>
+      <div className="flex justify-between items-center">
+        <img className="w-[120px]" src={sunIcon} alt="sun" />
+        <p className="text-[96px] font-bold">20°</p>
+      </div>
+    </div>
+  );
 }
