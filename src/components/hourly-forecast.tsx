a716a07549e096eb27679c859eea2b3ed75b87b2
Mickael Keita <keitamickael@gmail.com>
2025-09-25 15:50:10 +0200

    add hourly forecast display zone

--- src/components/hourly-forecast.tsx
+++ src/components/hourly-forecast.tsx
@@ -1,3 +1,30 @@
+import dorpdownLogo from "../assets/images/icon-dropdown.svg";
+import data from "../data/hourly-forecast";
+
 export function HourlyForecast() {
-  return <div className="hourly-forecast">HourlyForecast</div>;
+  return (
+    <div className="hourly-forecast px-4 py-5 xl:w-[384px]">
+      <div className="flex justify-between items-center mb-4">
+        <h3 className="text-xl font-semibold">Hourly forecast</h3>
+        <div className="flex items-center gap-2 px-4 py-2 rounded-[8px] bg-[var(--neutral-600)] cursor-pointer">
+          <p>Tuesday</p>
+          <img src={dorpdownLogo} alt="dorpdown logo" />
+        </div>
+      </div>
+      <ul className="flex flex-col gap-4">
+        {data.map((item) => (
+          <li
+            className="flex justify-between items-center px-3 py-2.5 rounded-[8px] bg-[var(--neutral-700)]"
+            key={item.time}
+          >
+            <div className="flex items-center gap-2">
+              <img className="w-10" src={item.icon} alt={item.time} />
+              <p>{item.time} PM</p>
+            </div>
+            <p>{item.temp}°</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/data/hourly-forecast.ts
+++ src/data/hourly-forecast.ts
@@ -1,54 +1,53 @@
 import drizzleIcon from "../assets/images/icon-drizzle.webp";
 import fogIcon from "../assets/images/icon-fog.webp";
+import overcastIcon from "../assets/images/icon-overcast.webp";
 import snowIcon from "../assets/images/icon-snow.webp";
 import rainIcon from "../assets/images/icon-rain.webp";
 import stormIcon from "../assets/images/icon-storm.webp";
 import sunnyIcon from "../assets/images/icon-sunny.webp";
 import partlyCloudyIcon from "../assets/images/icon-partly-cloudy.webp";
 
-const data = [
+const timelyData = [
   {
-    day: "Mon",
-    icon: sunnyIcon,
-    min: 18,
-    max: 28,
-  },
-  {
-    day: "Tue",
-    icon: drizzleIcon,
-    min: 17,
-    max: 25,
-  },
-  {
-    day: "Wed",
-    icon: rainIcon,
-    min: 16,
-    max: 23,
-  },
-  {
-    day: "Thu",
-    icon: stormIcon,
-    min: 19,
-    max: 27,
-  },
-  {
-    day: "Fri",
-    icon: partlyCloudyIcon,
-    min: 20,
-    max: 29,
-  },
-  {
-    day: "Sat",
+    time: "8",
     icon: fogIcon,
-    min: 15,
-    max: 22,
+    temp: 14,
   },
   {
-    day: "Sun",
+    time: "9",
+    icon: drizzleIcon,
+    temp: 15,
+  },
+  {
+    time: "10",
+    icon: rainIcon,
+    temp: 16,
+  },
+  {
+    time: "11",
+    icon: overcastIcon,
+    temp: 17,
+  },
+  {
+    time: "12",
+    icon: partlyCloudyIcon,
+    temp: 19,
+  },
+  {
+    time: "13",
+    icon: sunnyIcon,
+    temp: 22,
+  },
+  {
+    time: "14",
+    icon: stormIcon,
+    temp: 20,
+  },
+  {
+    time: "15",
     icon: snowIcon,
-    min: -2,
-    max: 4,
+    temp: 3,
   },
 ];
 
-export default data;
+export default timelyData;
